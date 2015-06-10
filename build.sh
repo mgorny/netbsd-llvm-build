c17c17b1a6b0dcc1fbb1808be86635d0834bc9d6
Vince Harron <vharron@google.com>
2015-06-10 08:59:20 -0700

    Created a working build script that depends on prebuilt projects
    Change-Id: I1cc59bc634cbd84bd51553d7bbec2fa6a73259c1

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 ROOT=$(pwd)
 export PATH=$PATH:$ROOT/clang/bin
 
-#ln -s external/clang llvm/tools/clang
-#ln -s external/lldb llvm/tools/lldb
+ln -s ../../clang llvm/tools/clang
+ln -s ../../lldb llvm/tools/lldb
 
-mkdir -p out
-cd out
-cmake -GNinja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_LINKER=ld.gold -DCMAKE_BUILD_TYPE=Debug ../external/llvm
-ninja
+CONFIG=Release
+PRE=$ROOT/prebuilts
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+BUILD=$ROOT/out/host
+rm -rf $BUILD
+mkdir -p $BUILD
+cd $BUILD
+LLDB_FLAGS="-fuse-ld=gold -target x86_64-unknown-linux"
+CLANG=$PRE/clang/linux-x86/bin/clang
+export SWIG_LIB=$PRE/swig/linux-x86/share/swig/2.0.11/
+$PRE/cmake/linux-x86/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_C_COMPILER="$CLANG"  -DCMAKE_CXX_COMPILER="$CLANG++" -DCMAKE_C_FLAGS="$LLDB_FLAGS" -DCMAKE_CXX_FLAGS="$LLDB_FLAGS" -DSWIG_EXECUTABLE=$PRE/swig/linux-x86/bin/swig $ROOT/llvm
+$PRE/ninja/linux-x86/ninja
+$PRE/ninja/linux-x86/ninja check-lldb
+
