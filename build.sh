7d0f1cb341f883a5f267b6cacc770f0c9bf46f34
Vince Harron <vharron@google.com>
2015-06-11 14:56:18 -0700

    Updated build script to work with studio-* manifest.
    Moved external repos into external/
    Moved clang into host/3.6
    
    Change-Id: I8af47252006f91432d79ab44140726bc756b65ff

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
+#!/bin/bash -ex
 ROOT=$(pwd)
-export PATH=$PATH:$ROOT/clang/bin
-
-ln -s ../../clang llvm/tools/clang
-ln -s ../../lldb llvm/tools/lldb
+ln -s ../../clang external/llvm/tools/clang || true
+ln -s ../../lldb external/llvm/tools/lldb || true
 
 CONFIG=Release
 PRE=$ROOT/prebuilts
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-BUILD=$ROOT/out/host
+BUILD=$ROOT/out/lldb/host
 rm -rf $BUILD
 mkdir -p $BUILD
 cd $BUILD
 LLDB_FLAGS="-fuse-ld=gold -target x86_64-unknown-linux"
-CLANG=$PRE/clang/linux-x86/bin/clang
+CLANG=$PRE/clang/linux-x86/host/3.6/bin/clang
 export SWIG_LIB=$PRE/swig/linux-x86/share/swig/2.0.11/
-$PRE/cmake/linux-x86/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_C_COMPILER="$CLANG"  -DCMAKE_CXX_COMPILER="$CLANG++" -DCMAKE_C_FLAGS="$LLDB_FLAGS" -DCMAKE_CXX_FLAGS="$LLDB_FLAGS" -DSWIG_EXECUTABLE=$PRE/swig/linux-x86/bin/swig $ROOT/llvm
+$PRE/cmake/linux-x86/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_C_COMPILER="$CLANG"  -DCMAKE_CXX_COMPILER="$CLANG++" -DCMAKE_C_FLAGS="$LLDB_FLAGS" -DCMAKE_CXX_FLAGS="$LLDB_FLAGS" -DSWIG_EXECUTABLE=$PRE/swig/linux-x86/bin/swig $ROOT/external/llvm
 $PRE/ninja/linux-x86/ninja
 $PRE/ninja/linux-x86/ninja check-lldb
 
