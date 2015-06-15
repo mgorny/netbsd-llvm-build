1e10de60e9df307fdb080d4b3dffcc1740367134
Vince Harron <vharron@google.com>
2015-06-15 15:25:05 -0700

    Refactored Darwin build script to conform to build script standard
    Specifically modeled after tools/idea/build_studio.sh
    
    Also added zip step
    
    Change-Id: Iefebedf8798cce8fa96f1b32f9f9e1d7da5b3204

--- build-darwin.sh
+++ build-darwin.sh
@@ -1,27 +1,75 @@
-#!/bin/bash -ex
+#!/bin/bash -x
+# Expected arguments:
+# $1 = out_dir
+# $2 = dist_dir
+# $3 = build_number
+
+# exit on error
+set -e
+
 # OSX lacks a "realpath" bash command
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-# $0 == external/lldb-utils/build-darwin.sh
-ROOT=$(dirname $(dirname $(dirname $(realpath "$0"))))
+# calculate the root directory from the script path
+# this script lives two directories down from the root
+# external/lldb-utils/build.sh
+ROOT_DIR="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
 
-# OUT isn't used yet
-OUT=$(realpath "$1")
+function die() {
+  echo "$*" > /dev/stderr
+  echo "Usage: $0 <out_dir> <dest_dir> <build_number>" > /dev/stderr
+  exit 1
+}
 
-if [ ! -f external/llvm/tools/clang ]; then
-    ln -s ../../clang external/llvm/tools/clang
-fi
-if [ ! -f external/lldb/llvm ]; then
-    ln -s ../llvm external/lldb/llvm
-fi
+while [[ -n "$1" ]]; do
+  if [[ -z "$OUT" ]]; then
+    OUT="$1"
+  elif [[ -z "$DIST" ]]; then
+    DIST="$1"
+  elif [[ -z "$BNUM" ]]; then
+    BNUM="$1"
+  else
+    die "[$0] Unknown parameter: $1"
+  fi
+  shift
+done
+
+if [[ -z "$OUT" ]]; then die "## Error: Missing out folder"; fi
+if [[ -z "$DIST" ]]; then die "## Error: Missing destination folder"; fi
+if [[ -z "$BNUM" ]]; then die "## Error: Missing build number"; fi
+
+cd "$ROOT_DIR"
+
+mkdir -p "$OUT"
+
+echo "## Building android-studio ##"
+echo "## Dest dir : $DIST"
+echo "## Qualifier: $QUAL"
+echo "## Build Num: $BNUßM"
+echo
+
+ln -s ../../clang external/llvm/tools || true
+ln -s ../llvm external/lldb || true
+
+BUILD="$ROOT_DIR/$OUT/lldb/host"
+rm -rf "$BUILD"
+rm -rf "$DIST"
+mkdir -p "$BUILD"
+
+export SWIG_LIB=$PRE/swig/darwin-x86/share/swig/2.0.11/
+INSTALL=$ROOT_DIR/$OUT/lldb/install
 
 CONFIG=Release
-PRE=$ROOT/prebuilts
+PRE="$ROOT_DIR/prebuilts"
 export PATH="$PRE/swig/darwin-x86/bin:$PATH"
 export SWIG_LIB=$PRE/swig/darwin-x86/share/swig/2.0.11/
-cd $ROOT/external/lldb
-xcodebuild -configuration $CONFIG -target desktop
-cd $ROOT/external/lldb/test
-./dosep.py
+cd $ROOT_DIR/external/lldb
+xcodebuild -configuration $CONFIG -target desktop OBJROOT="$BUILD" SYMROOT="$BUILD"
+
+cd $ROOT_DIR/external/lldb/test
+./dosep.py -o "-m --executable $BUILD/$CONFIG/lldb -s $BUILD/traces"
+
+mkdir -p "$ROOT_DIR/$DIST/"
+(cd $BUILD/$CONFIG/ && zip -r - lldb LLDB.framework) > "$ROOT_DIR/$DIST/lldb-mac-$BNUM.zip"
