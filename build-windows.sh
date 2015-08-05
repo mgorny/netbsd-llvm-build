ef2d8c97f8491a78565215462c6fe08ae06e6694
Chaoren Lin <chaorenl@google.com>
2015-08-05 14:23:55 -0700

    Update build-windows.sh.
    Change-Id: I15ff5e23c87fb5786aa058d94c4b42e381746549

--- build-windows.sh
+++ build-windows.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Expected arguments:
 # $1 = out_dir
-# $2 = dist_dir
+# $2 = dest_dir
 # $3 = build_number
 #
 # Dependencies:
 # cygwin zip
-
 
 # exit on error
 set -e
 
 # calculate the root directory from the script path
 # this script lives two directories down from the root
-# external/lldb-utils/build.sh
-ROOT_DIR="$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")"
+# external/lldb-utils/build-windows.sh
+ROOT_DIR="$(cygpath -w "$(readlink -f "$(dirname "$0")/../..")")"
+cd "$ROOT_DIR"
 
 function die() {
   echo "$*" > /dev/stderr
@@ -22,72 +22,66 @@ function die() {
   exit 1
 }
 
-while [[ -n "$1" ]]; do
-  if [[ -z "$OUT" ]]; then
-    OUT="$1"
-  elif [[ -z "$DIST" ]]; then
-    DIST="$1"
-  elif [[ -z "$BNUM" ]]; then
-    BNUM="$1"
-  else
-    die "[$0] Unknown parameter: $1"
-  fi
-  shift
-done
+(($# > 3)) && die "[$0] Unknown parameter: $4"
 
-if [[ -z "$OUT" ]]; then die "## Error: Missing out folder"; fi
-if [[ -z "$DIST" ]]; then die "## Error: Missing destination folder"; fi
-if [[ -z "$BNUM" ]]; then die "## Error: Missing build number"; fi
+OUT="$1"
+DEST="$2"
+BNUM="$3"
 
-cd "$ROOT_DIR"
+[ ! "$OUT"  ] && die "## Error: Missing out folder"
+[ ! "$DEST" ] && die "## Error: Missing destination folder"
+[ ! "$BNUM" ] && die "## Error: Missing build number"
 
-mkdir -p "$OUT"
+OUT="$(cygpath -w "$(readlink -f "$OUT")")"
+DEST="$(cygpath -w "$(readlink -f "$DEST")")"
 
-echo "## Building android-studio ##"
-echo "## Dest dir : $DIST"
-echo "## Qualifier: $QUAL"
-echo "## Build Num: $BNUM"
-echo
+cat <<END_INFO
+## Building android-studio ##
+## Out Dir  : $OUT
+## Dest Dir : $DEST
+## Build Num: $BNUM
 
-mv external/clang external/llvm/tools/clang || true
-mv external/lldb external/llvm/tools/lldb || true
+END_INFO
+
+LLVM="$ROOT_DIR"'\external\llvm'
+LLDB="$ROOT_DIR"'\external\lldb'
+CLANG="$ROOT_DIR"'\external\clang'
+
+PRE="$ROOT_DIR"'\prebuilts'
+CMAKE="$PRE"'\cmake\windows-x86\bin\cmake'
+NINJA="$PRE"'\ninja\windows-x86\ninja'
+
+export SWIG_LIB="$PRE"'\swig\windows-x86\share\swig\2.0.11'
+
+INSTALL="$OUT"'\lldb\install'
+rm -rf "$INSTALL"
 
 CONFIG=Release
-PRE="$ROOT_DIR/prebuilts"
 
-export PATH="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/bin":"/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/":"/cygdrive/c/Program Files (x86)/Windows Kits/8.1/bin/x86":"$PATH"
-export SWIG_LIB="D:\\src\\tmp\\prebuilts\\swig\\windows-x86\\share\\swig\\2.0.11"
-export INCLUDE="C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\INCLUDE;C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\ATLMFC\\INCLUDE;C:\\Program Files (x86)\\Windows Kits\\8.1\\include\\shared;C:\\Program Files (x86)\\Windows Kits\\8.1\\include\\um;C:\\Program Files (x86)\\Windows Kits\\8.1\\include\\winrt;"
-export LIB="C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\LIB;C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\ATLMFC\\LIB;C:\\Program Files (x86)\\Windows Kits\\8.1\\lib\\winv6.3\\um\\x86;"
-export LIBPATH="C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319;C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\LIB;C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\ATLMFC\\LIB;C:\\Program Files (x86)\\Windows Kits\\8.1\\References\\CommonConfiguration\\Neutral;C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v8.1\\ExtensionSDKs\\Microsoft.VCLibs\\12.0\\References\\CommonConfiguration\\neutral;"
-
-export PATH="$PRE/ninja/windows-x86/":"$PATH"
-
-BUILD="$ROOT_DIR/$OUT/lldb/host"
+BUILD="$OUT"'\lldb\host'
 rm -rf "$BUILD"
 mkdir -p "$BUILD"
-cd "$BUILD"
-INSTALL=$ROOT_DIR/$OUT/lldb/install
 
-export SWIG_LIB=$(cygpath -w $PRE/swig/windows-x86/share/swig/2.0.11/)
+unset CMAKE_OPTIONS
+CMAKE_OPTIONS+=(-GNinja)
+CMAKE_OPTIONS+=("$LLVM")
+CMAKE_OPTIONS+=(-DCMAKE_BUILD_TYPE=$CONFIG)
+CMAKE_OPTIONS+=(-DSWIG_DIR="$PRE"'\swig\windows-x86')
+CMAKE_OPTIONS+=(-DSWIG_EXECUTABLE="$PRE"'\swig\windows-x86\bin\swig.exe')
+CMAKE_OPTIONS+=(-DPYTHON_HOME="$PRE"'\python\windows-x86\x86')
+CMAKE_OPTIONS+=(-DLLVM_TARGETS_TO_BUILD="ARM;X86;AArch64;Mips")
+CMAKE_OPTIONS+=(-DCMAKE_INSTALL_PREFIX="$INSTALL")
+CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_LLDB_SOURCE_DIR="$LLDB")
+CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_CLANG_SOURCE_DIR="$CLANG")
 
-$PRE/cmake/windows-x86/bin/cmake -GNinja \
--DCMAKE_BUILD_TYPE=$CONFIG \
--DPYTHON_EXECUTABLE="$(cygpath -w $PRE/python/windows-x86/x86/python)" \
--DPYTHON_HOME="$(cygpath -w $PRE/python/windows-x86/x86)" \
--DLLVM_TARGETS_TO_BUILD="ARM;X86;AArch64;Mips" \
--DSWIG_DIR="$(cygpath -w $PRE/swig/windows-x86)" \
--DSWIG_EXECUTABLE="$(cygpath -w $PRE/swig/windows-x86/bin/swig)" \
--DCMAKE_INSTALL_PREFIX="$(cygpath -w $INSTALL)" \
-"$(cygpath -w $ROOT_DIR/external/llvm)"
+unset CMD
+CMD+=(cmd /c 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat')
+CMD+=('&&' cd "$BUILD")
+CMD+=('&&' "$CMAKE" "${CMAKE_OPTIONS[@]}")
+CMD+=('&&' "$NINJA" lldb finish_swig)
+CMD+=('&&' "$NINJA" install)
 
-$PRE/ninja/windows-x86/ninja lldb finish_swig
+PATH="$(cygpath -up "$(dirname "$NINJA")"';C:\Windows\system32')" "${CMD[@]}"
 
-$PRE/ninja/windows-x86/ninja install
-
-#cd $ROOT_DIR/external/lldb/test
-#./dosep.py -o "-m --executable $BUILD/bin/lldb -s $BUILD/traces"
-
-mkdir -p $DIST
-# zip file is 5.5GB, need to prune
-#(cd $INSTALL && zip -r - ".") > "$DIST/lldb-windows-$BNUM.zip"
+mkdir -p "$DEST"
+(cd "$INSTALL" && zip -r "$DEST"'\'lldb-windows-${BNUM}.zip .)
