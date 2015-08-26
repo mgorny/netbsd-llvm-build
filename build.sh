0f4bd11dc3d8022c203f0d03881eb8c3907be3f6
Chaoren Lin <chaorenl@google.com>
2015-08-25 18:56:09 -0700

    Make build.sh a common entry point.
    Change-Id: I1738f8ad42c997233600b69ebf789e88b714a5df

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# delete this script once the build servers are pointing directly to
-# external/lldb-utils/build-linux.sh
-external/lldb-utils/build-linux.sh $@
+
+# TODO: move common parts here
+
+case "$(uname -s)" in
+	Linux)  OS=linux;;
+	Darwin) OS=darwin;;
+	*_NT-*) OS=windows;;
+esac
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+source "$ROOT_DIR/external/lldb-utils/build-${OS}.sh" "$@"
