e4a10d4f7a0865b76c7a55ca3ade3936c05497d0
Pavel Labath <labath@google.com>
2016-02-01 17:11:46 +0000

    build-python: List projects which are known to build explicitly
    I've been experimenting with building python with VS2015, and I've
    found it hard to verify the build does not regress due to the
    oportunistic nature of the build script. This changes the script
    to list the projects to build explicitly, which enables us to
    check the build for errors.
    
    I've verified that the new script produces the same set of files.
    
    Change-Id: Ic841820db698c60f69264cd14b6bf51faac45a73

--- prebuilts/build-python.sh
+++ prebuilts/build-python.sh
@@ -22,11 +22,17 @@ case "$OS" in
 windows)
 	cp PC/pyconfig.h Include/
 	devenv PCbuild/pcbuild.sln /Upgrade
-	# some projects will fail and that's okay
-	devenv PCbuild/pcbuild.sln /Build Debug || true
-	devenv PCbuild/pcbuild.sln /Build Release || true
-	devenv PCbuild/pcbuild.sln /Build "Debug^|x64" || true
-	devenv PCbuild/pcbuild.sln /Build "Release^|x64" || true
+        # Not all projects will build (due to missing dependencies), so we build only a selected
+        # set. The rest are not needed for our purposes anyway.
+	for project in _ctypes _ctypes_test _elementtree _multiprocessing _socket _testcapi
+		bdist_wininst kill_python make_buildinfo make_versioninfo pyexpat python
+		pythoncore pythonw select unicodedata w9xpopen winsound; do
+
+		devenv PCbuild/pcbuild.sln /Build Debug /Project $project
+		devenv PCbuild/pcbuild.sln /Build Release /Project $project
+		devenv PCbuild/pcbuild.sln /Build "Debug^|x64" /Project $project
+		devenv PCbuild/pcbuild.sln /Build "Release^|x64" /Project $project
+	done
 	curl -L http://llvm.org/svn/llvm-project/lldb/trunk/scripts/install_custom_python.py -o install_custom_python.py
 	python install_custom_python.py --source "$(cygpath -w "$RD/$BASE")" --dest "$(cygpath -w "$INSTALL")" --overwrite --silent
 	;;
