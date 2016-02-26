c55cf60586a451ad0c245191b54f0ae33cc9f1d3
Chaoren Lin <chaorenl@google.com>
2016-02-26 14:43:43 -0800

    Don't need to specifically exclude projects by platform.
    They should fail gracefully.
    
    Change-Id: I82efb23aafd77c2a8211b9bacee2b350bab0fbdb

--- prebuilts/build-all.sh
+++ prebuilts/build-all.sh
@@ -2,11 +2,8 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
-case "$(uname)" in
-	Linux|Darwin) "$SCRIPT_DIR/build-libedit.sh" "$@";;
-esac
-
 "$SCRIPT_DIR/build-cmake.sh" "$@"
+"$SCRIPT_DIR/build-libedit.sh" "$@"
 "$SCRIPT_DIR/build-ninja.sh" "$@"
 "$SCRIPT_DIR/build-python.sh" "$@"
 "$SCRIPT_DIR/build-swig.sh" "$@"
