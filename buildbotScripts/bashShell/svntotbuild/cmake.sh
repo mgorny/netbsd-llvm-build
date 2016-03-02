3992433988819eaa23174809d0e2e4f6c57c3557
Pavel Labath <labath@google.com>
2016-03-02 10:56:55 +0000

    Revert "Switch linux svn builder to debug builds"
    This reverts commit d43f853adcffe92ace13fbffa26896ba2050cc14.
    
    Change-Id: I1a03e654c674d56d9f470c4f236416977910be8a

--- buildbotScripts/bashShell/svntotbuild/cmake.sh
+++ buildbotScripts/bashShell/svntotbuild/cmake.sh
@@ -17,6 +17,6 @@ else
       cmake -GNinja -DCMAKE_BUILD_TYPE=Release $llvmDir -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DLLDB_DISABLE_CURSES:BOOL=TRUE
     fi
   else
-    cmake -GNinja -DCMAKE_BUILD_TYPE=Debug $llvmDir -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    cmake -GNinja -DCMAKE_BUILD_TYPE=Release $llvmDir -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
   fi
 fi
