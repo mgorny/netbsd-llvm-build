af714664a6919c55505320db061f268a957a54ef
Jesik Min <jesikmin@google.com>
2016-06-23 15:19:50 -0700

    Automate NDK upgrade on LLDB buildbots and create toolchains with the new NDK
    Usage: ./updateToolChain.sh [-c]
    *** If the option argument -c is given, then the NDK bundle is downloaded from a
    canary server.
    *** Return 0 if completes successfully; return a negative number otherwise.
    
    UPDATE AS OF 06/29/2016, 12:57
      -> Corrected spelling mistakes in comments and expanded channel_id option to
      0, 1, 2, and 3
    
    UPDATE AS OF 06/29/2016, 11:41
      -> Reformatted comments
      -> Rename NDK_BUNDLE_PATH_DIR to NDK_BUNDLE_ID and use the variable in both directory
      managing and SDKDownloader command
      -> Modified -c binary optional arugument into [-c/-channel 0/1] option and
      added [-h/--help] option as well
      -> Modified minor spelling erros in comments
      -> Refactored getNDKVersion function
    
    UPDATE AS OF 06/27/2016, 18:30
      -> Added "set -e" command in line 2
      -> Check properties file in the $toolchain directory and copy the properties
      file whenever the toolchain creation process was successful
    
    UPDATE AS OF 06/27/2016, 15:54
      -> Modified indentation issues
      -> Modified some minor issues
      -> Revert to use make-standalone-toolchain.sh in order to support clang cross
      compiling
      -> Modified the script to accept the architecture with the string "-clang" in
      its name
    
    UPDATE AS OF 06/27/2016, 11:32
      -> Modified handleStagedToolchains function to not move the newly created
      toolchain to toolchain_latest unless all creations succeeded
      -> Modified the part where removing toolchain_latest before creation is
      successful as we want to build with the original(existing) toolchain
    
    UPDATE AS OF 06/27/2016, 10:40
      -> Added arm arm 9,16,21 to the
      buildbotScripts/testCfg/arch_api_preset_linux.cfg file
      -> Moved linux-specific environment variables to updateToolChain.sh
      -> Modified minor issues in updateToolChain.sh and made it skip unnecessary
      toolchain creation if NDK version is already up-to-date
    
    1) Remove the existing NDK directory and download the latest version, default
    from the public server.
    2) Create toolchains with the newly downloaded NDK. All the presets are to be
    stored and modified in testCfg/arch_api_preset file.
    3) Each newly created toolchain is first stored in the temporary directory as
    set in setEnv.sh
    4) If each creation is successful, then copy all the toolchains in the
    temporary directory to the permanent directory (which is also set in setEnv.sh)
    and remove the entire temporary directory.
    5) If any one of the creations was unsuccessful, then just remove the whole
    temporary directory and leave the permanent directory intact.
    
    Change-Id: I22388c27eb7db022f3fed6085a585d03fa33bde5

--- buildbotScripts/bashShell/svntotbuild/setEnv.sh
+++ buildbotScripts/bashShell/svntotbuild/setEnv.sh
@@ -5,7 +5,18 @@ export originalDir=$(pwd)
 export rootDir=$(pwd)/..
 export buildDir=$rootDir/build
 export remoteDir=/data/local/tmp/lldb
-export toolchain=$HOME/Toolchains_r11c
+
+dataRoot=""
+if [ ! -d "/lldb-buildbot" ]; then #check whether the build server has lldb-buildbot
+  dataRoot=$HOME
+else
+  dataRoot="/lldb-buildbot"
+fi
+echo "DATAROOT"$dataRoot
+
+export toolchain=$dataRoot/Toolchains_latest
+export sdkDir=$dataRoot/Sdk
+
 export port=5430
 export gstrace=gs://lldb_test_traces
 export gsbinaries=gs://lldb_binaries
---
+++ buildbotScripts/bashShell/svntotbuild/updateToolChain.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+set -e
+source setEnv.sh
+
+# constants for directory and file names
+NDK_BUNDLE_ID="ndk-bundle"
+SOURCE_PROPERTIES_FILENAME="source.properties"
+tempToolchain=$dataRoot/Toolchains_stage
+updateLib=$rootDir/scripts/lldb-utils/buildbotScripts
+
+DOWNLOAD_CHANNEL=0 # set default channel as a stable channel
+
+# option handling ([-c or --channel] and [-h or --help])
+if [[ $# -ne 2 && $# -ne 0 ]] && [[ $# -eq 1 && ( $1 != "-help" && $1 != "-h" ) ]]; then
+  echo "Invalid number of options or arguments: $param (enter option -h or --help for detail)" >&2
+  exit -1
+fi
+
+while [[ $# -gt 0 ]]; do
+  param="$1"
+  case $param in
+    -c|--channel)
+      DOWNLOAD_CHANNEL="$2"
+
+      # check whether channel_id parameter is one of 0, 1, 2, or 3
+      if [[ ! "$DOWNLOAD_CHANNEL" -ge 0 || ! "$DOWNLOAD_CHANNEL" -le 3 || ! "$DOWNLOAD_CHANNEL" =~ ^[0-9]+$ ]]; then
+        echo "Invalid argument: channel_id argument must be one of 0, 1, 2, or 3"
+        exit -1
+      fi
+      shift
+      ;;
+    -h|--help)
+      echo "Usage: [-c <channel_id>] or [--channel <channel_id>] for choosing from which server to download"
+      echo "Example: ./updateToolChain.sh -c 0  or  ./updateToolChain.sh --channel 1"
+      echo "Note: If no optional argument is given, default channel is set to 0 or the stable server"
+      shift
+      ;;
+    -*)
+      echo "Invalid option: $param (enter option -h or --help for detail)" >&2
+      exit -1
+      ;;
+    *)
+      echo "Invalid argument: $param (enter option -h or --help for detail)" >&2
+      exit -1
+      ;;
+  esac
+  shift
+done
+
+# check whether the environment variable for sdk path is set properly or not
+if [ ! -d "$sdkDir" ]
+then
+  echo "ERROR: Need to reset sdkDir environment variable (see setEnv.sh)">&2
+  exit -2
+fi
+
+echo "Starting ndk update..."
+
+EXISTING_PKG_REVISION_NUMBER="" # variable for storing the existing package revision number
+
+# get NDK version from source.properties file
+function getNDKVersion {
+  PROPERTIES_FILE=$1
+  echo $(grep Pkg.Revision $PROPERTIES_FILE | cut -d '=' -f2 | sed 's/^ *//g' | sed 's/ *$//g')
+}
+
+# retrieve from which NDK version the existing toolchains were created
+if [ -e "$toolchain"/"$SOURCE_PROPERTIES_FILENAME" ]; then
+  EXISTING_PKG_REVISION_NUMBER=$(getNDKVersion $toolchain"/"$SOURCE_PROPERTIES_FILENAME)
+  echo "Existing ndk-bundle detected... version-"$EXISTING_PKG_REVISION_NUMBER
+else
+  echo "No existing ndk-bundle detected..."
+fi
+
+# download the newest version of ndk
+cd $updateLib
+java -cp lib/repository.jar:lib/sdklib.jar:lib/commons-compress-1.0.jar:lib/common.jar:lib/guava-17.0.jar:lib/httpcore-4.4.1.jar:lib/httpclient-4.4.1.jar:lib/commons-logging-1.2.jar com.android.sdklib.tool.SdkDownloader --channel=$DOWNLOAD_CHANNEL $sdkDir $NDK_BUNDLE_ID
+cd $sdkDir
+
+# print the package revision number (i.e. version number)
+NEW_PKG_REVISION_NUMBER=$(getNDKVersion $sdkDir"/"$NDK_BUNDLE_ID"/"$SOURCE_PROPERTIES_FILENAME)
+echo "New ndk-bundle installed: version-"$NEW_PKG_REVISION_NUMBER
+
+# check whether the creation of new toolchains is required
+if [ "$EXISTING_PKG_REVISION_NUMBER" == "$NEW_PKG_REVISION_NUMBER" ] && [ -d "$toolchain" ]
+then
+  echo "NDK and toolchains are already up to date..."
+  echo "No toochain creation required... Done"
+  exit 0
+fi
+
+cd $sdkDir"/"$NDK_BUNDLE_ID
+
+# function that put newly created toolchains to the temporary directory and copy them to the permanent directory if successful
+function handleStagedtoolchains {
+  API=$1
+  EXIT_CODE=$2
+  if [ $EXIT_CODE -eq 0 ]; then
+    echo "  -> toolchain for "$ARCHITECTURE":""API "$API" created"
+  else
+    echo "  ERROR: fail to create a toolchain for "$ARCHITECTURE":""API "$API >&2
+    rm -rf $tempToolchain
+    exit -3
+  fi
+}
+
+# function that reads the file that contains presets and creates toolchain in accordance with the preset values
+function createToolChains {
+  cat $updateLib"/testCfg/arch_api_preset.cfg" | while read LINE
+  do
+    echo $LINE
+    IFS=' ' read -ra TOKENS <<< "$LINE"
+    ARCHITECTURE=${TOKENS[0]}
+    INSTALL_DIR=${TOKENS[1]}
+    APIS=${TOKENS[2]}
+    IFS=',' read -ra APITOKENS <<< "$APIS"
+    for API in "${APITOKENS[@]}"; do
+      INSTALL_DIR=$INSTALL_DIR"-"$API
+      if [ $( echo $ARCHITECTURE | grep clang ) ]; then
+        INSTALL_DIR=$INSTALL_DIR"-clang"
+      fi
+      ./build/tools/make-standalone-toolchain.sh --platform=android-$API --toolchain=$ARCHITECTURE --install-dir=$tempToolchain"/"$INSTALL_DIR
+      EXIT_CODE=$?
+      set -e; handleStagedtoolchains $API $EXIT_CODE; set +e
+    done
+
+    # make-alone-toolchain.sh is to be deprecated from r13; usage of alternative python script
+    # make sure python script creating toolchains is executable before running
+    # chmod +x ./build/tools/make_standalone_toolchain.py
+    # ./build/tools/make_standalone_toolchain.py  --arch $ARCHITECTURE --api $API --install-dir $tempToolchain"/"$INSTALL_DIR-$API --force
+
+  done
+}
+
+echo "Start creating toolchains..."
+
+createToolChains
+ctResult=$? # store the exit code of createToolChains
+
+# check whether createToolChains function is failed or not
+if [ $ctResult -ne 0 ]; then
+  echo "FATAL ERROR: Toolchain creation process failed">&2
+  exit -3
+else # if succeeded, remove the previous toolchains and rename the temporary directory to the permanent one
+  rm -rf $toolchain; mv $tempToolchain $toolchain; cp $sdkDir"/"$NDK_BUNDLE_ID"/"$SOURCE_PROPERTIES_FILENAME $toolchain"/"$SOURCE_PROPERTIES_FILENAME
+fi
+
+echo "Toolchain update was successfully completed!"
