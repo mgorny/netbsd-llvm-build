a810288b80a31599f5cd77344aab2199019cab01
Pavel Labath <labath@google.com>
2016-07-04 14:19:47 +0100

    Enable logging in lldb-server for the android tests
    Log files will be written to the device. LLDB test suite already
    knows how to collect them and store them with the other (client)
    logs.
    
    Change-Id: I9dd3a715daa477ab73c3f84879dd6f40c769784b

--- buildbotScripts/bashShell/svntotbuild/androidTest.sh
+++ buildbotScripts/bashShell/svntotbuild/androidTest.sh
@@ -41,7 +41,12 @@ else
   listen_url=127.0.0.1:$port
   connect_url=connect://$deviceId:$port
 fi
-screen -d -m adb -s $deviceId shell TMPDIR=$remoteDir/tmp $remoteDir/lldb-server platform --listen $listen_url --server
+screen -d -m adb -s "$deviceId" shell \
+    env \
+    TMPDIR="$remoteDir/tmp" \
+    LLDB_DEBUGSERVER_LOG_FILE=server.log \
+    LLDB_SERVER_LOG_CHANNELS="gdb-remote packets:lldb all" \
+    "$remoteDir/lldb-server" platform --listen $listen_url --server
 
 export LLDB_TEST_THREADS=8
 
