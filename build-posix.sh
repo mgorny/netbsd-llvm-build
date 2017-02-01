47e52e44dd59f2b13ad42eb58861a1eb2fac71af
Pavel Labath <labath@google.com>
2017-02-01 00:01:51 +0000

    Switch linux host build to use the cmake cache file
    This enables one to replicate the host build by just doing
    $ cmake -C config/linux.cmake
    
    Change-Id: Ia9c2e7442c7544eeaa3d682b1a0858b33ff60e2e

--- build-posix.sh
+++ build-posix.sh
@@ -17,78 +17,39 @@ export PATH=/usr/bin:/bin
 
 CONFIG=Release
 
-unset LLDB_FLAGS
-unset LLDB_LINKER_FLAGS
 unset CMAKE_OPTIONS
 unset CMAKE_TARGETS
 
 CMAKE_TARGETS+=(lldb)
-CMAKE_TARGETS+=(lldb-server)
-CMAKE_TARGETS+=(finish_swig)
 if [ "$OS" == linux ]; then
-	CC=$PREBUILTS/clang/linux-x86/host/3.6/bin/clang
-	TOOLCHAIN=$PREBUILTS/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8
-        EXEC_PREFIX=$TOOLCHAIN/bin/x86_64-linux-
-
-	LLDB_FLAGS+=(-fuse-ld=gold)
-	LLDB_FLAGS+=(-target x86_64-unknown-linux)
-	LLDB_FLAGS+=(--gcc-toolchain="$TOOLCHAIN")
-	LLDB_FLAGS+=(--sysroot="$TOOLCHAIN/sysroot")
-	LLDB_FLAGS+=(-B"$EXEC_PREFIX")
-	LLDB_FLAGS+=(-I"$PREBUILTS/libedit/linux-x86/include")
-
-	LLDB_LINKER_FLAGS+=(-L"$PREBUILTS/libedit/linux-x86/lib")
-
-	CMAKE_OPTIONS+=(-DPYTHON_EXECUTABLE="$PYTHON_DIR/bin/python")
-	CMAKE_OPTIONS+=(-DPYTHON_LIBRARY="$PYTHON_DIR/lib/libpython2.7.so")
-	CMAKE_OPTIONS+=(-DPYTHON_INCLUDE_DIR="$PYTHON_DIR/include/python2.7")
-	CMAKE_OPTIONS+=(-DLLVM_ENABLE_EH=YES)
-	CMAKE_OPTIONS+=(-DLLVM_ENABLE_RTTI=YES)
-
-	# Necessary because clang recognizes x86_64-linux-gnu
-	# as a valid gcc toolchain but not x86_64-linux.
-	find "$TOOLCHAIN" -name x86_64-linux -exec ln -fns {} {}-gnu \;
-	function finish() {
-		find "$TOOLCHAIN" -name x86_64-linux-gnu -type l -delete
-	}
-	trap finish EXIT
-
-	CMAKE_TARGETS+=(lib/readline.so)
+	CMAKE_OPTIONS+=(-C"$LLDB_UTILS/config/linux.cmake")
 else
 	# Allow the user to override the compiler used. Unfortunately, the prebuilt clang does
 	# not work correctly on the user machines. This makes it impossible to reproduce the
 	# buildbot builds exactly, but this will at least enable us to use the same script.
 	CC=${LLDB_OVERRIDE_CC:-"$PREBUILTS/clang/darwin-x86/sdk/3.5/bin/clang"}
-	EXEC_PREFIX=
 
         CMAKE_OPTIONS+=(-DLLDB_CODESIGN_IDENTITY="")
+	CMAKE_OPTIONS+=(-DCMAKE_C_COMPILER="$CC")
+	CMAKE_OPTIONS+=(-DCMAKE_CXX_COMPILER="$CC++")
+	CMAKE_OPTIONS+=(-GNinja)
+	CMAKE_OPTIONS+=(-DCMAKE_MAKE_PROGRAM="$NINJA")
+	CMAKE_OPTIONS+=(-DCMAKE_BUILD_TYPE=$CONFIG)
+	CMAKE_OPTIONS+=(-DLLDB_DISABLE_CURSES=1)
+	CMAKE_OPTIONS+=(-DSWIG_EXECUTABLE="$SWIG_DIR/bin/swig")
+	CMAKE_OPTIONS+=(-DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64;Mips;Hexagon")
+	CMAKE_OPTIONS+=(-DCMAKE_INSTALL_PREFIX=)
+	CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_LLDB_SOURCE_DIR="$LLDB")
+	CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_CLANG_SOURCE_DIR="$CLANG")
+	CMAKE_OPTIONS+=(-DCMAKE_AR="ar")
 fi
 
-CMAKE_OPTIONS+=(-GNinja)
 CMAKE_OPTIONS+=(-H"$LLVM")
 CMAKE_OPTIONS+=(-B"$BUILD")
-CMAKE_OPTIONS+=(-DCMAKE_MAKE_PROGRAM="$NINJA")
-CMAKE_OPTIONS+=(-DCMAKE_BUILD_TYPE=$CONFIG)
-CMAKE_OPTIONS+=(-DCMAKE_C_COMPILER="$CC")
-CMAKE_OPTIONS+=(-DCMAKE_CXX_COMPILER="$CC++")
-CMAKE_OPTIONS+=(-DCMAKE_C_FLAGS="${LLDB_FLAGS[*]}")
-CMAKE_OPTIONS+=(-DCMAKE_CXX_FLAGS="${LLDB_FLAGS[*]}")
-CMAKE_OPTIONS+=(-DCMAKE_EXE_LINKER_FLAGS="${LLDB_LINKER_FLAGS[*]}")
-CMAKE_OPTIONS+=(-DCMAKE_MODULE_LINKER_FLAGS="${LLDB_LINKER_FLAGS[*]}")
-CMAKE_OPTIONS+=(-DCMAKE_SHARED_LINKER_FLAGS="${LLDB_LINKER_FLAGS[*]}")
-CMAKE_OPTIONS+=(-DCMAKE_AR="${EXEC_PREFIX}ar")
-CMAKE_OPTIONS+=(-DLLDB_DISABLE_CURSES=1)
-CMAKE_OPTIONS+=(-DSWIG_EXECUTABLE="$SWIG_DIR/bin/swig")
-CMAKE_OPTIONS+=(-DLLVM_TARGETS_TO_BUILD="X86;ARM;AArch64;Mips;Hexagon")
-CMAKE_OPTIONS+=(-DCMAKE_INSTALL_PREFIX=)
-CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_LLDB_SOURCE_DIR="$LLDB")
-CMAKE_OPTIONS+=(-DLLVM_EXTERNAL_CLANG_SOURCE_DIR="$CLANG")
 
 
 "$CMAKE" "${CMAKE_OPTIONS[@]}"
-for TARGET in "${CMAKE_TARGETS[@]}"; do
-	"$CMAKE" --build "$BUILD" --target "$TARGET"
-done
+"$CMAKE" --build "$BUILD" --target lldb
 
 # install target builds/installs 5G of stuff we don't need
 #DESTDIR="$INSTALL/host" "$CMAKE" --build "$BUILD" --target install
@@ -96,6 +57,8 @@ done
 mkdir -p "$INSTALL/host/"{bin,lib,include/lldb,include/LLDB}
 cp -a "$BUILD/bin/"lldb*                             "$INSTALL/host/bin/"
 if [ "$OS" == linux ]; then
+	TOOLCHAIN=$PREBUILTS/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8
+
 	cp -a "$PYTHON_DIR/bin/"python*                      "$INSTALL/host/bin/"
 	cp -a "$BUILD/lib/"{liblldb.so*,readline.so}         "$INSTALL/host/lib/"
 	cp -a "$PREBUILTS/libedit/linux-x86/lib/"libedit.so* "$INSTALL/host/lib/"
--- config/linux.toolchain.cmake
+++ config/linux.toolchain.cmake
@@ -7,10 +7,13 @@ set(CMAKE_CXX_COMPILER "${PREBUILTS}/clang/linux-x86/host/3.6/bin/clang++")
 set(CMAKE_SYSROOT "${TOOLCHAIN}/sysroot")
 set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
   "${TOOLCHAIN}/x86_64-linux/include/c++/4.8"
-  "${TOOLCHAIN}/x86_64-linux/include/c++/4.8/x86_64-linux")
+  "${TOOLCHAIN}/x86_64-linux/include/c++/4.8/x86_64-linux"
+  "${PREBUILTS}/libedit/linux-x86/include"
+  )
 set(CMAKE_C_STANDARD_LIBRARIES_INIT
 "-L${TOOLCHAIN}/lib/gcc/x86_64-linux/4.8 \
 -L${TOOLCHAIN}/x86_64-linux/lib64 \
+-L${PREBUILTS}/libedit/linux-x86/lib \
 -B${TOOLCHAIN}/bin/x86_64-linux- \
 -B${TOOLCHAIN}/lib/gcc/x86_64-linux/4.8/")
 set(CMAKE_CXX_STANDARD_LIBRARIES_INIT "${CMAKE_C_STANDARD_LIBRARIES_INIT}")
@@ -18,3 +21,4 @@ set(CMAKE_C_COMPILER_TARGET x86_64-unknown-linux)
 set(CMAKE_CXX_COMPILER_TARGET x86_64-unknown-linux)
 set(CMAKE_C_COMPILER_EXTERNAL_TOOLCHAIN ${TOOLCHAIN})
 set(CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN ${TOOLCHAIN})
+set(_CMAKE_TOOLCHAIN_PREFIX "${TOOLCHAIN}/bin/x86_64-linux-")
