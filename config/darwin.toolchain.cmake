9d235be09c4703ca5355afbc23cb6c028c448fbc
Pavel Labath <labath@google.com>
2017-02-20 15:02:49 +0000

    cmake: Set the OSX deployment target in the darwin toolchain file
    The minimum version supported by the studio is 10.10. I set it to 10.9,
    just to be safe. We can bump this up once it starts becoming a
    maintenance burden.
    
    Change-Id: I59a266fc613dbf2cfbc5b24795cc8879ab00c1d6

--- config/darwin.toolchain.cmake
+++ config/darwin.toolchain.cmake
@@ -1,1 +1,3 @@
-# Unfortunately our darwin build is not very hermetic, so this file is empty
+# Unfortunately our darwin build is not very hermetic, so this file is almost empty
+
+set(CMAKE_OSX_DEPLOYMENT_TARGET 10.9 CACHE STRING "Minimum OSX version")
