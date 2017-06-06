8278531a0c5546f8eee6c143ae468a842b76bafc
Pavel Labath <labath@google.com>
2017-06-06 10:16:51 +0100

    Improve libedit building script
    libedit is not supported on windows, so do not try to build it there.
    Also, improve the "touch hack" to make sure the timestamps are in the
    correct order.
    
    Change-Id: I9bee6a64b6ac628f248f5101ab6e253d22535138

--- prebuilts/build-libedit.sh
+++ prebuilts/build-libedit.sh
@@ -11,15 +11,30 @@ source "$SCRIPT_DIR/build-common.sh" "$@"
 
 set -x
 
+if [ "$OS" == windows ]; then
+  exit 0
+fi
+
 pushd "$BUILD"
+
+# Avoid autoconf dependency
+#
+# git doesn't preserve timestamps (for good reason) and libedit build is set up
+# in a way that it will attempt to regenerate configure files, which will fail
+# if we don't have autoconf installed.
+# There's no reason to do this as we have not modified the files, so just touch
+# them in the correct dependency sequence to avoid this.
+touch "$SOURCE"/{configure.ac,acinclude.m4}
+sleep 2
+touch "$SOURCE"/aclocal.m4
+sleep 2
+touch "$SOURCE"/{.,doc,examples,src}/Makefile.in
+sleep 2
+touch "$SOURCE"/{configure,config.h.in}
 
 # libtool doesn't pass CFLAGS to link command
 "$SOURCE/configure" --prefix= CC="$CC $CFLAGS" CFLAGS=
 
-# avoid needing aclocal-1.15
-# configure modifies itself, which leads make into thinking that these need to
-# be regenerated
-touch "$SOURCE"/{aclocal.m4,configure.ac,Makefile.am,Makefile.in}
 
 make -j"$CORES"
 make DESTDIR="$INSTALL" install-strip
