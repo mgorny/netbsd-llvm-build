5eea8f546807c3fecbc2600b59b0fb09631b48fb
Pavel Labath <labath@google.com>
2017-06-07 11:14:53 +0100

    Make mac sdk search more robust
    ab buildbots only have sdk 10.10, fully updated workstations tend to have
    10.12. We don't really care about which one we use as our minimum deployment
    target is 10.9, so just update the toolchain file to search for one of them.
    Although yucky, This is consistent with what the platform build does.
    
    Change-Id: If3810a1bc74b0df5f3abda1b6142c9720ec55e10

--- config/darwin.toolchain.cmake
+++ config/darwin.toolchain.cmake
@@ -5,14 +5,22 @@ set(CMAKE_OSX_DEPLOYMENT_TARGET 10.9 CACHE STRING "Minimum OSX version")
 set(CMAKE_C_COMPILER "${PREBUILTS}/clang/host/darwin-x86/clang-4053586/bin/clang")
 set(CMAKE_CXX_COMPILER "${PREBUILTS}/clang/host/darwin-x86/clang-4053586/bin/clang++")
 
-execute_process(COMMAND xcrun --sdk macosx10.12 --show-sdk-path
-  RESULT_VARIABLE XCRUN_RESULT
-  OUTPUT_VARIABLE XCRUN_STDOUT
-  ERROR_VARIABLE XCRUN_STDERR
-  OUTPUT_STRIP_TRAILING_WHITESPACE)
-if(NOT (${XCRUN_RESULT} EQUAL 0))
-  message(FATAL_ERROR "xcrun command failed with result ${XCRUN_RESULT} and stderr:\n${XCRUN_STDERR}")
+foreach(SDK 10.12 10.11 10.10)
+  execute_process(COMMAND xcrun --sdk macosx${SDK} --show-sdk-path
+    RESULT_VARIABLE XCRUN_RESULT
+    OUTPUT_VARIABLE XCRUN_STDOUT
+    ERROR_VARIABLE XCRUN_STDERR
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message(STATUS
+    "xcrun looking for sdk ${SDK}. Stdout:\n${XCRUN_STDOUT}\nstderr:\n${XCRUN_STDERR}")
+  if(${XCRUN_RESULT} EQUAL 0)
+    break()
+  endif()
+endforeach()
+if(NOT(${XCRUN_RESULT} EQUAL 0))
+  message(FATAL_ERROR "Unable to find a suitable sdk")
 endif()
+
 set(CMAKE_SYSROOT ${XCRUN_STDOUT})
 
 set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES "${TOOLCHAIN}/include/c++/v1")
