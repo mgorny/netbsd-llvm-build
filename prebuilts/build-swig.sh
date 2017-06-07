d4e6ab3553bf51df39352c4e0dbe6d4c4fc25fb3
Pavel Labath <labath@google.com>
2017-06-07 17:55:10 +0100

    [prebuilts] Fix script for building swig 3.0.12
    running autogen.sh will not work on the buildbots, as they don't have
    necessary autoconf (nor bison)  tools installed. Instead, I run those
    locally and patch them in when needed (emulating a build from a
    distribution tarball, which does not require these tools). autoconf and
    bison generate text files, so checking these in is not too gross, and it
    still enables us to build swig binaries on the buildbots.
    
    Change-Id: Iace75e72217b463a028feeca3c320b20b790ce34

--- prebuilts/build-swig.sh
+++ prebuilts/build-swig.sh
@@ -9,15 +9,13 @@ PROJECT=swig
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 source "$SCRIPT_DIR/build-common.sh" "$@"
 
+set -e -x
 PCRE=$EXTERNAL/pcre
 DEPENDENCIES+=("$PCRE")
 
 git -C "$PCRE" archive @:dist \
 	--format=tar --prefix=pcre-git/ -o "$BUILD/pcre-git.tar"
 
-pushd "$SOURCE"
-./autogen.sh
-popd
 pushd "$BUILD"
 "$SOURCE/Tools/pcre-build.sh"
 "$SOURCE/configure" --prefix=
