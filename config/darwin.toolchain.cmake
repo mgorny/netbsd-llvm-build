fab1ee317df4f483e2a2ab91263050899be7739c
Pavel Labath <labath@google.com>
2017-11-01 21:30:27 +0000

    Bump up minimum osx version
    Android Studio 3.0 already requires macosx10.10.
    
    I don't see any practical benefit from doing this now, but it might save
    us some hassle in the future.
    
    Change-Id: Idfb0d3c028f9684f71d257705acf9bca402c51b0

--- config/darwin.toolchain.cmake
+++ config/darwin.toolchain.cmake
@@ -1,3 +1,3 @@
 # Unfortunately our darwin build is not very hermetic, so this file is almost empty
 
-set(CMAKE_OSX_DEPLOYMENT_TARGET 10.9 CACHE STRING "Minimum OSX version")
+set(CMAKE_OSX_DEPLOYMENT_TARGET 10.10 CACHE STRING "Minimum OSX version")
