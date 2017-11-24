e3c6dc77ff0b290baec5d559d81731820d226c25
Pavel Labath <labath@google.com>
2017-11-24 17:13:18 +0000

    Update upstream buildbots to ndk r16
    Change-Id: I7a5a1e511b7ba7974393aba9e23660f11d85b771

--- buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
+++ buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
@@ -8,7 +8,7 @@ SET buildDir=%rootDir%\build
 SET pythonHome=C:\Users\lldb_build\ll\prebuilts\python-2015\x86
 SET remoteDir=/data/local/tmp/lldb
 
-SET ANDROID_NDK_HOME=C:\android-ndk-r15b
+SET ANDROID_NDK_HOME=C:\android-ndk-r16
 SET port=5430
 SET lockDir=c:\tmp\lock\lldbbuild.exclusivelock
 SET PATH=%PATH%;%pythonHome%
