391a61976678051e9f9a1ce901cda3b0ad9b26c5
Pavel Labath <labath@google.com>
2017-12-11 14:58:01 +0000

    buildbot scripts: build 64-bit lldb on windows
    The next step would be to get ms build tools on the bot and use them the
    same way as we do on ab, but this at least makes sure we build with the
    same bitwidth.
    
    Change-Id: I29460da767a65768bb72d5eaaf0c9a865cf14455

--- buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
+++ buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
@@ -5,11 +5,11 @@ SET llvmDir=%rootDir%\llvm
 SET lldbDir=%llvmDir%\tools\lldb
 SET clangDir=%llvmDir%\tools\clang
 SET buildDir=%rootDir%\build
-SET pythonHome=C:\Users\lldb_build\ll\prebuilts\python-2015\x86
+SET pythonHome=C:\Users\lldb_build\ll\prebuilts\python-2015\x64
 SET remoteDir=/data/local/tmp/lldb
 
 SET ANDROID_NDK_HOME=C:\android-ndk-r15b
 SET port=5430
 SET lockDir=c:\tmp\lock\lldbbuild.exclusivelock
 SET PATH=%PATH%;%pythonHome%
-call "%VS140COMNTOOLS%\VsDevCmd.bat"
+call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
