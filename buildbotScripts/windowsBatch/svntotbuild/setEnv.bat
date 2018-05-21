5ec1c4a11649da37738e327134294cdf0f655218
Pavel Labath <labath@google.com>
2018-05-21 15:07:42 +0100

    Update buildbots to NDK r17
    Change-Id: I20af49bf3ac80693677a0707e3d8ef8fd6892b6d

--- buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
+++ buildbotScripts/windowsBatch/svntotbuild/setEnv.bat
@@ -8,7 +8,7 @@ SET buildDir=%rootDir%\build
 SET pythonHome=C:\Users\lldb_build\ll\prebuilts\python-2015\x64
 SET remoteDir=/data/local/tmp/lldb
 
-SET ANDROID_NDK_HOME=C:\android-ndk-r16
+SET ANDROID_NDK_HOME=C:\android-ndk-r17
 SET port=5430
 SET lockDir=c:\tmp\lock\lldbbuild.exclusivelock
 SET PATH=%PATH%;%pythonHome%
