6988617f655e9251da43c65bbb498c1c650be299
Michał Górny <mgorny@gentoo.org>
2018-11-30 18:49:43 +0100

    Add periodic status pings to keep the build alive

--- buildbotScripts/bashShell/svntotbuild/buildLocal.sh
+++ buildbotScripts/bashShell/svntotbuild/buildLocal.sh
@@ -3,6 +3,17 @@ set -e
 source setEnv.sh
 source cleanUp.sh
 
+# add periodical status 'pings'
+run_status_pings() {
+	while :; do
+		sleep 300
+		date '+[%F %T]'
+	done
+}
+run_status_pings &
+st_ping_pid=$!
+trap 'kill "${st_ping_pid}"' EXIT
+
 set -x
 nice -n 10 ninja -C "$buildDir"
 markBuildComplete
