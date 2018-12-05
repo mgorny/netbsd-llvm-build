e90f81d2361bcd608fdc6e52bbcc0819ed982763
Michał Górny <mgorny@gentoo.org>
2018-12-05 17:56:37 +0100

    More correct RPATH setup

--- buildbotScripts/bashShell/svntotbuild/cmake.sh
+++ buildbotScripts/bashShell/svntotbuild/cmake.sh
@@ -15,7 +15,8 @@ if [[ "$host" == NetBSD ]]; then
   # TODO: /usr/pkg/lib rpath needs to be appended after builddir
   cmake -GNinja -DCMAKE_BUILD_TYPE="$buildType" "$llvmDir" \
     -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
-    -DCMAKE_{BUILD,INSTALL}_RPATH=/usr/pkg/lib
+    -DCMAKE_BUILD_RPATH="${PWD}/lib;/usr/pkg/lib" \
+    -DCMAKE_INSTALL_RPATH=/usr/pkg/lib
 elif [[ "$host" == Linux ]]; then
   cmake -GNinja -DCMAKE_BUILD_TYPE="$buildType" "$llvmDir" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_EH=YES -DLLVM_ENABLE_RTTI=YES "$@"
 else
