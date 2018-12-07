7d0c16a73bdded7123bbd5d812e33d720cd960ba
Kamil Rytarowski <n54@gmx.com>
2018-12-07 09:07:57 +0100

    Enable check-lld

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -3,4 +3,4 @@ set -e
 source setEnv.sh
 
 set -x
-nice -n 10 ninja -C "$buildDir" check-lit check-llvm check-clang check-clang-tools
+nice -n 10 ninja -C "$buildDir" check-lit check-llvm check-clang check-clang-tools check-lld
