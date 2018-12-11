20031fb1fd212e3074216dd8d7053e5b0721efc9
Michał Górny <mgorny@gentoo.org>
2018-12-11 22:25:12 +0100

    Keep-running tests even if one of the suites fail

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -3,6 +3,6 @@ set -e
 source setEnv.sh
 
 set -x
-nice -n 10 ninja -C "$buildDir" check-lit check-llvm check-clang \
+nice -n 10 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-lld check-polly check-unwind \
 	check-libcxxabi check-openmp
