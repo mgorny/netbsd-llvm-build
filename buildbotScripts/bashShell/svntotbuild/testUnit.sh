d4e9829300b7f6550bf40e62fc7a4ab87ec6b350
Michał Górny <mgorny@gentoo.org>
2018-12-11 23:25:29 +0100

    Skip OpenMP tests until we resolve driver/libc++ problems

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -5,4 +5,4 @@ source setEnv.sh
 set -x
 nice -n 10 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-lld check-polly check-unwind \
-	check-libcxxabi check-openmp
+	check-libcxxabi # check-openmp
