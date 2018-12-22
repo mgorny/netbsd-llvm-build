5eb173566fa252e97f761d96f773ae92ca47fbad
Michał Górny <mgorny@gentoo.org>
2018-12-22 11:56:25 +0100

    Reenable test-openmp

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -4,7 +4,7 @@ source setEnv.sh
 
 set -x
 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
-	check-clang-tools check-polly check-unwind \
-	check-libcxxabi check-libcxx # check-lld test-suite check-openmp
+	check-clang-tools check-polly check-unwind check-openmp \
+	check-libcxxabi check-libcxx # check-lld test-suite
 
 # check-openmp and test-suite trigger lib(std)c++
