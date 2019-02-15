288dc335a30087f21b73c02ccec01aa5d5e95822
Michał Górny <mgorny@gentoo.org>
2019-02-15 20:30:37 +0100

    Include lldb unittests
    Let's see if gdb-remote fix resolves flaky tests.

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -5,4 +5,5 @@ source setEnv.sh
 set -x
 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-polly check-unwind check-openmp \
-	check-libcxxabi check-libcxx check-lld # test-suite
+	check-libcxxabi check-libcxx check-lld check-lldb-unit
+	# test-suite
