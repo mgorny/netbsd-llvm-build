5bf718516166733b15430e7663ff717a15ae85a1
Michał Górny <mgorny@gentoo.org>
2019-03-06 15:27:24 +0100

    Enable full lldb test suite

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -5,5 +5,5 @@ source setEnv.sh
 set -x
 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-polly check-unwind check-openmp \
-	check-libcxxabi check-libcxx check-lld check-lldb-unit
+	check-libcxxabi check-libcxx check-lld check-lldb-lit
 	# test-suite
