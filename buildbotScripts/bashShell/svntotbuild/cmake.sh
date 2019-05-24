76ae5c1e4992c8f4f6b1a04209f53b561d29a451
Michał Górny <mgorny@gentoo.org>
2019-05-24 21:57:45 +0200

    Try reducing job count even more
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- buildbotScripts/bashShell/svntotbuild/cmake.sh
+++ buildbotScripts/bashShell/svntotbuild/cmake.sh
@@ -22,7 +22,7 @@ if [[ "$host" == NetBSD ]]; then
     -DOPENMP_TEST_FLAGS="-cxx-isystem${PWD}/include/c++/v1"
 
   # reduce job count to make lldb tests more stable
-  sed -i -e '/COMMAND.*lit.*lldb\/lit$/s:-vv:-j2 -vv:' build.ninja
+  sed -i -e '/COMMAND.*lit.*lldb\/lit$/s:-vv:-j1 -vv:' build.ninja
 elif [[ "$host" == Linux ]]; then
   cmake -GNinja -DCMAKE_BUILD_TYPE="$buildType" "$llvmDir" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_EH=YES -DLLVM_ENABLE_RTTI=YES "$@"
 else
