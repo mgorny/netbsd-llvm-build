7248ee7714c5d89985547304ea7b1c349623f9bc
Michał Górny <mgorny@gentoo.org>
2019-07-11 17:32:49 +0200

    cmake: Revert libc++abi changes as they break normal builds

--- buildbotScripts/bashShell/svntotbuild/cmake.sh
+++ buildbotScripts/bashShell/svntotbuild/cmake.sh
@@ -13,11 +13,13 @@ cd "$buildDir"
 host=$(uname)
 if [[ "$host" == NetBSD ]]; then
   # TODO: /usr/pkg/lib rpath needs to be appended after builddir
+  # NOTICE: change -DLIBCXX_CXX_ABI to 'libcxxabi' when upstream recommits
+  # 'Do not cleverly link against libc++abi just because it happens to be there'
   cmake -GNinja -DCMAKE_BUILD_TYPE="$buildType" "$llvmDir" \
     -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_BUILD_RPATH="${PWD}/lib;/usr/pkg/lib" \
     -DCMAKE_INSTALL_RPATH=/usr/pkg/lib \
-    -DLIBCXX_CXX_ABI=libcxxabi \
+    -DLIBCXX_CXX_ABI=default \
     -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
     -DLLVM_LIT_ARGS="-vv;--param;cxx_under_test=${PWD}/bin/clang++" \
     -DOPENMP_TEST_FLAGS="-cxx-isystem${PWD}/include/c++/v1"
