4541f2913ca1b22c9151e516d38b3a49a7e4eb86
Michał Górny <mgorny@gentoo.org>
2019-07-20 18:36:06 +0200

    Revert "testUnit: make FileCheck dump whole input on fail"
    Try if that fixes new test failures.

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -3,7 +3,6 @@ set -e
 source setEnv.sh
 
 set -x
-export FILECHECK_DUMP_INPUT_ON_FAILURE=1
 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-polly check-unwind check-openmp \
 	check-libcxxabi check-libcxx check-lld check-lldb-lit
