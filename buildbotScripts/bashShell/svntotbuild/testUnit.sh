5f798cb244e3a4b865119f00013acb87992fd6c3
Michał Górny <mgorny@gentoo.org>
2019-11-19 22:23:13 +0100

    Disable tests to investigate host failures better

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -4,6 +4,7 @@ source setEnv.sh
 
 set -x
 export FILECHECK_DUMP_INPUT_ON_FAILURE=1
+exit 0
 ninja -k 9999 -C "$buildDir" check-lit check-llvm check-clang \
 	check-clang-tools check-polly check-unwind check-openmp \
 	check-libcxxabi check-libcxx check-lld check-lldb-lit
