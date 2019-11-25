acfa6bc1f532b60da77a87d68104539f00e30bbb
Michał Górny <mgorny@gentoo.org>
2019-11-25 17:41:19 +0100

    Add separate clang build step to satisfy libc++ deps

--- buildbotScripts/bashShell/svntotbuild/buildLocal.sh
+++ buildbotScripts/bashShell/svntotbuild/buildLocal.sh
@@ -19,5 +19,6 @@ ninja -C "${buildDir}" \
 	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '^[^-]*TableGen$')
 ninja -C "${buildDir}" \
 	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '\.a$')
+ninja -j 4 -C "${buildDir}" clang
 ninja -j 4 -C "${buildDir}"
 markBuildComplete
