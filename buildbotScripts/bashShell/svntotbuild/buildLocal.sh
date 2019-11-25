a6d1d5b38aa2d5d607bdd7bf2c769c648d5dcfff
Michał Górny <mgorny@gentoo.org>
2019-11-25 18:10:53 +0100

    Try excluding libcxx from step 2

--- buildbotScripts/bashShell/svntotbuild/buildLocal.sh
+++ buildbotScripts/bashShell/svntotbuild/buildLocal.sh
@@ -18,7 +18,7 @@ set -x
 ninja -C "${buildDir}" \
 	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '^[^-]*TableGen$')
 ninja -C "${buildDir}" \
-	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '\.a$')
+	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '\.a$' | grep -v libcxx)
 ninja -j 4 -C "${buildDir}" clang
 ninja -j 4 -C "${buildDir}"
 markBuildComplete
