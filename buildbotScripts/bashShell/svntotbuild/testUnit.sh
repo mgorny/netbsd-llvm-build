357fd1488a62f788258357a062b18f4b339f8ec9
Michał Górny <mgorny@gentoo.org>
2019-11-29 20:39:43 +0100

    Disable more tools in stage1, skip tests for now

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -4,8 +4,8 @@ source setEnv.sh
 
 set -x
 export FILECHECK_DUMP_INPUT_ON_FAILURE=1
-ninja -k 9999 -C "$buildDir" check-lit check-clang \
-	check-clang-tools check-polly check-lld check-lldb
+#ninja -k 9999 -C "$buildDir" check-lit check-clang \
+#	check-clang-tools check-polly check-lld check-lldb
 #	check-unwind check-openmp check-libcxxabi check-libcxx
 # check-llvm \
 	# test-suite
