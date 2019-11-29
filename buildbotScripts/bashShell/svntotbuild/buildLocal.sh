32af5bede5948015da4b7d7c70d6939be747c8aa
Michał Górny <mgorny@gentoo.org>
2019-11-29 16:27:18 +0100

    Remove obsolete status pings

--- buildbotScripts/bashShell/svntotbuild/buildLocal.sh
+++ buildbotScripts/bashShell/svntotbuild/buildLocal.sh
@@ -3,17 +3,6 @@ set -e
 source setEnv.sh
 source cleanUp.sh
 
-# add periodical status 'pings'
-run_status_pings() {
-	while :; do
-		sleep 300
-		date '+[%F %T]'
-	done
-}
-run_status_pings &
-st_ping_pid=$!
-trap 'kill "${st_ping_pid}"' EXIT
-
 set -x
 ninja -C "${buildDir}" \
 	$(ninja -C "${buildDir}" -t targets all | cut -d: -f1 | grep '^[^-]*TableGen$')
