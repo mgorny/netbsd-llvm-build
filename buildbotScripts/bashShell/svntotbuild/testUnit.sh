dba2e86e38a6753791f02472920666fe494d3e0b
Michał Górny <mgorny@gentoo.org>
2019-12-09 17:49:05 +0100

    Enable verbose ninja for tests

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -4,7 +4,7 @@ source setEnv.sh
 
 set -x
 export FILECHECK_DUMP_INPUT_ON_FAILURE=1
-ninja -k 9999 -C "$build2Dir" check-lit check-clang \
+ninja -v -k 9999 -C "$build2Dir" check-lit check-clang \
 	check-clang-tools check-polly check-lld check-lldb
 	check-unwind check-openmp check-libcxxabi check-libcxx
 # check-llvm \
