1a54702dcf8b48f03c5c4abe726a43976ee40872
Michał Górny <mgorny@gentoo.org>
2019-12-14 21:59:59 +0100

    Enable more tests, make the list cleaner

--- buildbotScripts/bashShell/svntotbuild/testUnit.sh
+++ buildbotScripts/bashShell/svntotbuild/testUnit.sh
@@ -2,10 +2,37 @@
 set -e
 source setEnv.sh
 
+tests=(
+	check-lit
+#	check-llvm    -- panics talia
+	check-clang
+#	check-clangd  -- huge breakage; TODO
+	check-clang-tools
+	check-polly
+	check-lld
+	check-lldb
+	check-unwind
+	check-openmp
+	check-libcxxabi
+	check-libcxx
+
+	# compiler-rt
+	check-builtins
+#	check-interception -- can't find tests?!
+#	check-lsan   -- needs patching for asan
+#	check-ubsan  -- needs patching for asan/tsan
+	check-cfi
+	check-sanitizer
+#	check-fuzzer -- aslr
+	check-asan{,-dynamic}
+	check-msan # also some aslr cases
+	check-tsan
+	check-safestack
+	check-ubsan-minimal
+	check-profile
+#	check-xray -- mprotect
+)
+
 set -x
 export FILECHECK_DUMP_INPUT_ON_FAILURE=1
-ninja -v -k 9999 -C "$build2Dir" check-lit check-clang \
-	check-clang-tools check-polly check-lld check-lldb
-	check-unwind check-openmp check-libcxxabi check-libcxx
-# check-llvm \
-	# test-suite
+ninja -v -k 9999 -C "$build2Dir" "${tests[@]}"
